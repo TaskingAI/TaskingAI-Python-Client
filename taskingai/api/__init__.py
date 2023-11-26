14b32d8f4defc3521d0df2c00893bdd5cacf015c
JamesirYao <thehuiger@gmail.com>
2023-11-26 17:55:20 +0800

    feat: update apis & models

--- taskingai/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from taskingai.api.assistant_api import AssistantApi
-from taskingai.api.retrieval_api import RetrievalApi
